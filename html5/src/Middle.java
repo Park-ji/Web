4f690bd98b055946551e350721c4c5dfe0dd6d19
Park-Ji <qkrwlgus2568@gmail.com>
2019-12-02 17:52:23 +0900

    Web

---
+++ html5/src/Middle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Middle {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int x=scanner.nextInt(20);
+	int y=scanner.nextInt(30);
+	int z=scanner.nextInt(40);
+
+	System.out.println(x);
+//	
+//	System.out.println(x+", "+y+", "+z);
+//	if((y<=x && x<=z)||(z<=x && x<=y)) System.out.println(x);
+//	else if((x<=y && y<=z)||(z<=y && y<=x)) System.out.println(y);
+//	else System.out.println(z);
+
+}
+}
